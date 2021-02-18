217fde5e785a8e09463475cabe19d666a467813c
CodeFuller <CodeFuller@gmail.com>
2021-02-18 08:16:24 +0300

    Add Microsoft Application Insights for Kubernetes

--- TestWebApiApplication/TestWebApiApplication/Startup.cs
+++ TestWebApiApplication/TestWebApiApplication/Startup.cs
@@ -18,8 +18,10 @@ namespace TestWebApiApplication
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-
 			services.AddControllers();
+
+			services.AddApplicationInsightsTelemetry();
+			services.AddApplicationInsightsKubernetesEnricher();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
