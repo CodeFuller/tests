f7d43b737c457fb6673c189f79d40c3cd159a9e4
CodeFuller <CodeFuller@gmail.com>
2021-02-21 23:14:31 +0300

    Add k8s manifest for TestGrpcCaller

---
+++ TestWebApiApplication/TestGrpcCaller/deploy.bat
@@ -0,0 +1,20 @@
+echo off
+
+docker build -t test-grpc-caller:1.0.0 -f Dockerfile .. || goto :error
+docker tag test-grpc-caller:1.0.0 codefuller/test-grpc-caller:1.0.0 || goto :error
+docker push codefuller/test-grpc-caller:1.0.0 || goto :error
+
+kubectl apply -f TestGrpcCaller.yaml --namespace=test || goto :error
+
+echo [92mDeployed successfully![0m
+goto :EOF
+
+:error
+
+set exit_code=%errorlevel%
+if "%exit_code%"=="0" (
+    set exit_code=1
+)
+
+echo [91mDeployment has FAILED with the error #%exit_code%[0m
+exit /b %exit_code%
